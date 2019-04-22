2a2c2cba05b07e07aa8df836539380d3385f5614
tda1312 <tda1312@users.noreply.github.com>
2019-04-22 21:35:29 +0700

    re-draw low level design

--- group-8/score_record/function.js
+++ group-8/score_record/function.js
@@ -1,23 +1,25 @@
 function showHS(){
-	var oldScore = 2; //set demo game score
-	localStorage.setItem('Score', JSON.stringify(oldScore)); //save game score as string
+		var oldScore = 2; //set demo game score
+		localStorage.setItem('Score', JSON.stringify(oldScore)); //save game score as string
 
-	checkHS();
+		checkHS();
 }
 
 function checkHS(){
-	var highScore = localStorage.getItem('Score') !== null ?
-			JSON.parse(localStorage.getItem('Score')) : 0 //check if there's available score, if not then assign value 0
-	var userpoint = 1; //set random user score
+		//check if there's available score, if not then assign value 0
+		var highScore = localStorage.getItem('Score') !== null ?
+				JSON.parse(localStorage.getItem('Score')) : 0;
+		var userpoint = 1; //set random user score
 
-	if(userpoint > highScore){
-		highScore = userpoint; //change high score if user score > saved game score
-		localStorage.setItem('Score', JSON.stringify(highScore)); // save new high score
-	}
-	updateHighScore(highScore);
+		if (userpoint > highScore){
+				highScore = userpoint; //change high score if user score > saved game score
+				localStorage.setItem('Score', JSON.stringify(highScore)); // save new high score
+		}
+
+		updateHighScore(highScore);
 }
 
 function updateHighScore(highScore){
-	document.getElementById('scrdisplay').innerHTML = highScore; //print high score
-	document.getElementById('scrdiv').style.display= "block"; //show high score block
+		document.getElementById('scrdisplay').innerHTML = highScore; //print high score
+		document.getElementById('scrdiv').style.display = 'block'; //show high score block
 }
