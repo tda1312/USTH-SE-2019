8c967cad051c32093edae858883c7b3dd0fc1881
larycoder <lenhuchuhiep99@gmail.com>
2019-04-23 06:44:59 +0700

    fix linter of clock bar

--- group-8/clock_bar/class.js
+++ group-8/clock_bar/class.js
@@ -1,43 +1,39 @@
-let limited = 2000; // define valid time for 1 round (milisecond)
-let initTime; // define init time beginning count
-let fracTime; // percentage of time valid to answr 
-let timeUpdate = 50; // define  of fluild of progress
+let limited = 2000 // define valid time for 1 round (milisecond)
+let initTime // define init time beginning count
+let fracTime // percentage of time valid to answer
+let timeUpdate = 50 // define  of fluild of progress
 
 // manager of system clock
-function clockBarManager(){
-    
-    // define machenism of cooldown clock bar
-    this.timeProcess = function(){
+function clockBarManager () {
+  // define machenism of cooldown clock bar
+  this.timeProcess = function () {
+    // define begin cooldown
+    initTime = new Date().getTime()
 
-        // define begin cooldown
-        initTime = new Date().getTime();
+    // define time cooldown
+    var process = setInterval(function () {
+    // get current time
+      var now = new Date().getTime()
 
-        // define time cooldown
-        var process = setInterval(function(){
+      // find the distance between now and the init time
+      var distance = now - initTime
 
-            // get current time
-            var now = new Date().getTime();
+      if (distance > limited) {
+        clearInterval(process)
+        drawFluid('100%')
+        return 1
+      }
 
-            // find the distance between now and the init time
-            var distance = now - initTime;
+      // set percentage of time cooldown
+      fracTime = Math.round((distance / limited) * 100)
 
-           if (distance > limited){
-               clearInterval(process);
-               drawFluid("100%")
-               return;                
-           }
-
-            // set percentage of time cooldown
-            fracTime = Math.round((distance/limited)*100);
-
-            // draw percentage to page
-            drawFluid(fracTime+"%");
-
-        },timeUpdate);
-    };
+      // draw percentage to page
+      drawFluid(fracTime + '%')
+    }, timeUpdate)
+  }
 }
 
 // send message to page
-function drawFluid(Message){
-    document.getElementById("perCents").style.width = Message;
+function drawFluid (Message) {
+  document.getElementById('perCents').style.width = Message
 }
