2bbfe060db724e13100e932e2c2c465e8e5ac9b7
tda1312 <tda1312@users.noreply.github.com>
2019-04-22 20:45:17 +0700

    modify function.js

--- group-8/score_record/function.js
+++ group-8/score_record/function.js
@@ -1,13 +1,23 @@
-function highScore(){
-	var localStorageName = "record";
-	var highScore;
-	var test = 2;
+function showHS(){
+	var oldScore = 2; //set demo game score
+	localStorage.setItem('Score', JSON.stringify(oldScore)); //save game score as string
 
-	highScore = localStorage.getItem(localStorageName) == null ? 0 :
-        	                localStorage.getItem(localStorageName);
-	updateHighScore(test);
+	checkHS();
 }
 
-function updateHighScore(Test){
-	document.getElementByID('highscore').innerHTML = Test;
+function checkHS(){
+	var highScore = localStorage.getItem('Score') !== null ?
+			JSON.parse(localStorage.getItem('Score')) : 0 //check if there's available score, if not then assign value 0
+	var userpoint = 1; //set random user score
+
+	if(userpoint > highScore){
+		highScore = userpoint; //change high score if user score > saved game score
+		localStorage.setItem('Score', JSON.stringify(highScore)); // save new high score
+	}
+	updateHighScore(highScore);
+}
+
+function updateHighScore(highScore){
+	document.getElementById('scrdisplay').innerHTML = highScore; //print high score
+	document.getElementById('scrdiv').style.display= "block"; //show high score block
 }
