315e4a5ebf08189e8dcbbe25f6f06773881ddac0
larycoder <lenhuchuhiep99@gmail.com>
2019-04-27 18:38:12 +0700

    fix checkHS function to get usrScore

--- group-8/score_record/function.js
+++ group-8/score_record/function.js
@@ -7,10 +7,8 @@ function showHS () {
 
 function checkHS () {
   // check if there's available score, if not then assign value 0
-  var highScore = localStorage.getItem('Score') !== null ?
-    JSON.parse(localStorage.getItem('Score')) : 0;
-  var userpoint = 1 // set random user score
-
+  var highScore = localStorage.getItem('Score') !== null ? JSON.parse(localStorage.getItem('Score')) : 0
+  var userpoint = localStorage.getItem('usrScore') !== null ? JSON.parse(localStorage.getItem('usrScore')) : 0
   if (userpoint > highScore) {
     highScore = userpoint // change high score if user score > saved game score
     localStorage.setItem('Score', JSON.stringify(highScore)) // save new high score
