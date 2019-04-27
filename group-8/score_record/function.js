005e1858d2a598d5b3d12c490227df289cda889c
larycoder <lenhuchuhiep99@gmail.com>
2019-04-27 17:59:10 +0700

    fix linter score record

--- group-8/score_record/function.js
+++ group-8/score_record/function.js
@@ -1,25 +1,25 @@
-function showHS(){
-		var oldScore = 2; //set demo game score
-		localStorage.setItem('Score', JSON.stringify(oldScore)); //save game score as string
+function showHS () {
+  var oldScore = 2 // set demo game score
+  localStorage.setItem('Score', JSON.stringify(oldScore)) // save game score as string
 
-		checkHS();
+  checkHS()
 }
 
-function checkHS(){
-		//check if there's available score, if not then assign value 0
-		var highScore = localStorage.getItem('Score') !== null ?
-				JSON.parse(localStorage.getItem('Score')) : 0;
-		var userpoint = 1; //set random user score
+function checkHS () {
+  // check if there's available score, if not then assign value 0
+  var highScore = localStorage.getItem('Score') !== null ?
+    JSON.parse(localStorage.getItem('Score')) : 0;
+  var userpoint = 1 // set random user score
 
-		if (userpoint > highScore){
-				highScore = userpoint; //change high score if user score > saved game score
-				localStorage.setItem('Score', JSON.stringify(highScore)); // save new high score
-		}
+  if (userpoint > highScore) {
+    highScore = userpoint // change high score if user score > saved game score
+    localStorage.setItem('Score', JSON.stringify(highScore)) // save new high score
+  }
 
-		updateHighScore(highScore);
+  updateHighScore(highScore)
 }
 
-function updateHighScore(highScore){
-		document.getElementById('scrdisplay').innerHTML = highScore; //print high score
-		document.getElementById('scrdiv').style.display = 'block'; //show high score block
+function updateHighScore (highScore) {
+  document.getElementById('scrdisplay').innerHTML = highScore // print high score
+  document.getElementById('scrdiv').style.display = 'block' // show high score block
 }
