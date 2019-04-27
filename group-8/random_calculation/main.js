1b9aa0ba9766b1270b99a2f59102c6f6ed486a56
larycoder <lenhuchuhiep99@gmail.com>
2019-04-27 18:54:07 +0700

    fix linter of random_math

--- group-8/random_calculation/main.js
+++ group-8/random_calculation/main.js
@@ -1,62 +1,57 @@
-let score = 0;
-let rightAns;
-let wrongAns;
-let finalAnswer;
+let score = 0
+let rightAns
+let wrongAns
+let finalAnswer
 
-function genNew() {
-  var sign = ['+', '-', 'x', ':'];
-  var firstElement =  Math.floor(Math.random() * 10 + 1);
-  var secElement =  Math.floor(Math.random() * 10 + 1);
-  var operator = sign[Math.floor(Math.random() * 4)];
+function genNew () {
+  var sign = ['+', '-', 'x', ':']
+  var firstElement = Math.floor(Math.random() * 10 + 1)
+  var secElement = Math.floor(Math.random() * 10 + 1)
+  var operator = sign[Math.floor(Math.random() * 4)]
 
-  if (operator == '+') rightAns = firstElement + secElement;
-  else if (operator == '-') rightAns = firstElement - secElement;
-  else if (operator == 'x') rightAns = firstElement * secElement;
-  else if (operator == ':')  {
-    rightAns = firstElement / secElement;
-    rightAns = rightAns.toFixed(2);
+  if (operator === '+') rightAns = firstElement + secElement
+  else if (operator === '-') rightAns = firstElement - secElement
+  else if (operator === 'x') rightAns = firstElement * secElement
+  else if (operator === ':') {
+    rightAns = firstElement / secElement
+    rightAns = rightAns.toFixed(2)
   }
 
-  let saiso = [1, -1];
-  wrongAns = rightAns + saiso[Math.floor(Math.random() * 2)];
-  let answers = [rightAns, wrongAns];
+  let saiso = [1, -1]
+  wrongAns = rightAns + saiso[Math.floor(Math.random() * 2)]
+  let answers = [rightAns, wrongAns]
 
-  finalAnswer = answers[Math.floor(Math.random() * 2)];
-  document.getElementById('Calculation').innerHTML =  firstElement + ' ' +
-  operator + ' ' + secElement + ' ' + '=' + ' ' + finalAnswer;
+  finalAnswer = answers[Math.floor(Math.random() * 2)]
+  document.getElementById('Calculation').innerHTML = firstElement + ' ' +
+  operator + ' ' + secElement + ' ' + '=' + ' ' + finalAnswer
 }
 
-function checkTrue() {
-  if (finalAnswer == rightAns) {
-    score += 1;
-    document.getElementById('score').innerHTML = score;
+function checkTrue () {
+  if (finalAnswer === rightAns) {
+    score += 1
+    document.getElementById('score').innerHTML = score
   } else {
-    score = 0;
-    document.getElementById('score').innerHTML = score;
-    alert('You lost');
+    document.getElementById('score').innerHTML = score
   }
 
-  genNew();
+  genNew()
 }
 
-function checkFalse() {
-  if (finalAnswer == wrongAns) {
-    score += 1;
-    document.getElementById('score').innerHTML = score;
+function checkFalse () {
+  if (finalAnswer === wrongAns) {
+    score += 1
+    document.getElementById('score').innerHTML = score
   } else {
-    score = 0;
-    document.getElementById('score').innerHTML = score;
-    alert('You lost');
+    document.getElementById('score').innerHTML = score
   }
 
-  genNew();
+  genNew()
 }
 
-function displayScore() {
-  //var score=0;
-  return 'Score:' + score;
+function displayScore () {
+  return 'Score:' + score
 }
 
 function restart () {
-  score = 0;
+  score = 0
 }
